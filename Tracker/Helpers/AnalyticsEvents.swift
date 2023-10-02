2108d97dd2d4d8059f5ad042d9d59a1dccffe508
Aleksandr <125129763+Kosmonur@users.noreply.github.com>
2023-10-02 21:28:04 +0300

    Update AnalyticsEvents.swift

--- Tracker/Helpers/AnalyticsEvents.swift
+++ Tracker/Helpers/AnalyticsEvents.swift
@@ -6,3 +6,13 @@
 //
 
 import Foundation
+
+enum Events {
+    static var openMainScreenEvent = Event(name: "open", screen: "Main", item: "")
+    static var closeMainScreenEvent = Event(name: "close", screen: "Main", item: "")
+    static var clickAddTrackerEvent = Event(name: "click", screen: "Main", item: "add_track")
+    static var clickFilterButtonEvent = Event(name: "click", screen: "Main", item: "filter")
+    static var clickEditTrackerEvent = Event(name: "click", screen: "Main", item: "edit")
+    static var clickDeleteTrackerEvent = Event(name: "click", screen: "Main", item: "delete")
+    static var clickCompeteButtonEvent = Event(name: "click", screen: "Main", item: "track")
+}
